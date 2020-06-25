56179ff43498f99b440b0f9a1b981dbc9427c66f
larapollehn <lara.pollehn@googlemail.com>
2020-06-25 18:06:50 +0200

    repo data is stored and read from redis cache

--- src/services/ProjectServices.js
+++ src/services/ProjectServices.js
@@ -3,19 +3,28 @@ const log = require('../log/Logger');
 const axios = require('axios');
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const PROJECT_KEY = "project_data";
+const CACHE_TTL = 60 * 60 * 24 * 3;
 console.assert(GITHUB_TOKEN !== null && GITHUB_TOKEN !== undefined, 'GITHUB_TOKEN is not set');
 
 const getProjects = (expressRequest, expressResponse) => {
-
-    axios({
-        method: 'POST',
-        url: 'https://api.github.com/graphql',
-        headers: {
-            "Authorization": `Bearer ${GITHUB_TOKEN}`
-        },
-        data: {
-            query:
-                `query{
+    log.debug('Github pinned Repo Data is needed');
+    redisAccess.get(PROJECT_KEY, function (err, reply) {
+        log.debug('Redis Cache is checked for stored data')
+        if(reply){
+            log.debug('Repo Data was found in Cache and is returned');
+            expressResponse.send(JSON.parse(reply));
+        }else{
+            log.debug('Repo Data was NOT found in Cache and is now fetched from github APi with query')
+            axios({
+                method: 'POST',
+                url: 'https://api.github.com/graphql',
+                headers: {
+                    "Authorization": `Bearer ${GITHUB_TOKEN}`
+                },
+                data: {
+                    query:
+                        `query{
                   repositoryOwner(login: "larapollehn") {
                     ... on ProfileOwner {
                       pinnedItemsRemaining
@@ -53,18 +62,17 @@ const getProjects = (expressRequest, expressResponse) => {
                   }
                 }
             `
+                }
+            }).then((githubResponse) => {
+                log.debug('Github responded with',githubResponse.data);
+                redisAccess.setex(PROJECT_KEY, CACHE_TTL , JSON.stringify(githubResponse.data));
+                expressResponse.status(200).send(githubResponse.data);
+            }).catch((error) => {
+                log.error('Could not fetch project data', error)
+                expressResponse.status(500).send(error.message);
+            })
         }
-    }).then((githubResponse) => {
-        log.debug('Github responded with',githubResponse.data);
-        expressResponse.status(200).send(githubResponse.data);
-    }).catch((error) => {
-        log.error('Could not fetch project data', error)
-        expressResponse.status(500).send(error.message);
     })
-}
-
-function checkCache(request, response, next) {
-
 }
 
 module.exports = {
