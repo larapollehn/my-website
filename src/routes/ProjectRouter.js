3b70ee9d202b314891bb633abfd1dc5756dd8384
larapollehn <lara.pollehn@googlemail.com>
2020-06-25 17:56:08 +0200

    get project data from github qpi v4

--- src/routes/ProjectRouter.js
+++ src/routes/ProjectRouter.js
@@ -3,4 +3,6 @@ const projectService = require('./../services/ProjectServices');
 
 const projectRouter = express.Router();
 
+projectRouter.get('/projects', projectService.getProjects);
+
 module.exports = projectRouter;
--- src/services/ProjectServices.js
+++ src/services/ProjectServices.js
@@ -1,10 +1,69 @@
-const sqlAccess = require('../data/SQLAccess');
+const redisAccess = require('../data/RedisAccess');
+const log = require('../log/Logger');
+const axios = require('axios');
 
-const checkCache = (request, response) => {
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+console.assert(GITHUB_TOKEN !== null && GITHUB_TOKEN !== undefined, 'GITHUB_TOKEN is not set');
 
+const getProjects = (expressRequest, expressResponse) => {
+
+    axios({
+        method: 'POST',
+        url: 'https://api.github.com/graphql',
+        headers: {
+            "Authorization": `Bearer ${GITHUB_TOKEN}`
+        },
+        data: {
+            query:
+                `query{
+                  repositoryOwner(login: "larapollehn") {
+                    ... on ProfileOwner {
+                      pinnedItemsRemaining
+                      itemShowcase {
+                        items(first: 5) {
+                          totalCount
+                          edges {
+                            node {
+                              ... on RepositoryInfo{
+                                 homepageUrl}
+                              ... on Repository {
+                                id
+                                openGraphImageUrl 
+                                name
+                                description
+                                url
+                                 languages(first:3) {
+                                  totalCount
+                                  edges {
+                                    node {
+                                      name
+                                    }
+                                  }
+                                }
+                                    ... on RepositoryInfo{
+                                      homepageUrl
+                                    }
+                              }
+                            }
+                          }
+                        }
+                        hasPinnedItems
+                      }
+                    }
+                  }
+                }
+            `
+        }
+    }).then((githubResponse) => {
+        log.debug('Github responded with',githubResponse.data);
+        expressResponse.status(200).send(githubResponse.data);
+    }).catch((error) => {
+        log.error('Could not fetch project data', error)
+        expressResponse.status(500).send(error.message);
+    })
 }
 
-const getProjects = (request, response) => {
+function checkCache(request, response, next) {
 
 }
 
