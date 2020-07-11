c1ab7f28205b742c332b921946a5e13822c0ad09
larapollehn <lara.pollehn@googlemail.com>
2020-07-11 23:30:17 +0200

    made redis test async

--- tests/getIndex.test.js
+++ tests/getIndex.test.js
@@ -1,13 +1,11 @@
+const {describe, expect} = require("@jest/globals");
+
 const app = require("../app");
 const sqlAccess = require("../src/data/SQLAccess");
 const redisAccess = require("../src/data/RedisAccess");
 const request = require('supertest');
 
 describe('Test GET Index', () => {
-    beforeAll(done => {
-        done();
-    });
-
     test('Postgres db works', done => {
         request(app)
             .get('/')
@@ -17,12 +15,11 @@ describe('Test GET Index', () => {
             });
     });
 
-    test('redis works', done => {
-        redisAccess.setex('test', 1800, 'test case works');
-        redisAccess.get('test', (err, data) => {
-            expect(data).toBe("test case works");
-            done();
-        });
+    test('redis works', async done => {
+        await redisAccess.setex('test', 1800, 'test case works');
+        const data = await redisAccess.get('test');
+        expect(data).toBe("test case works");
+        done();
     })
 
     afterAll(done => {
