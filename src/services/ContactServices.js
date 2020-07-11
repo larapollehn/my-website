f133d5cf840d9866399648e4ba3791b75fe414e2
larapollehn <lara.pollehn@googlemail.com>
2020-07-11 21:10:40 +0200

    added comments to frontend

--- src/services/ContactServices.js
+++ src/services/ContactServices.js
@@ -10,6 +10,19 @@ const RECEIVER = process.env.RECEIVER;
 const HOST = process.env.HOST;
 const MUST_VERIFY_MESSAGE = process.env.MUST_VERIFY_MESSAGE;
 
+/**
+ * tranporter instance for sending mails with func notify
+ */
+const transporter = nodemailer.createTransport({
+    host: HOST,
+    port: 587,
+    secure: false,
+    auth: {
+        user: SENDER,
+        pass: EMAILPW,
+    },
+});
+
 console.assert(RECEIVER !== null && RECEIVER !== undefined, "Email Receiver is not set. Did you set the environment variable properly?");
 console.assert(SENDER !== null && SENDER !== undefined, "Email Sender is not set. Did you set the environment variable properly?");
 console.assert(EMAILPW !== null && EMAILPW !== undefined, "Email Sender's password is not set. Did you set the environment variable properly?");
@@ -23,16 +36,24 @@ log.debug("Contact service with SMTP host:", HOST);
 log.debug("Contact service with recaptcha's secret:", RECAPTCHA_TOKEN);
 log.debug("Contact service with receiver's email:", MUST_VERIFY_MESSAGE);
 
+/**
+ * verifies user with reCaptcha and processes contact message if successful
+ * @param expressRequest contains google recaptcha token and user input from contact form
+ * @param expressResponse
+ */
 const sendMail = (expressRequest, expressResponse) => {
     log.debug("Incoming message for owner from contact form");
     if (MUST_VERIFY_MESSAGE === 'true') {
         log.debug("Incoming message must be verified. Verification now.");
-        const obtainedToken = expressRequest.body["token"];
+
+        const obtainedToken = expressRequest.body["token"]; //token for verifying with google recaptcha
         log.debug("Obtained token to verify", obtainedToken);
+
+        // post to google to verify user
         axios.post('https://www.google.com/recaptcha/api/siteverify?' + 'secret=' + RECAPTCHA_TOKEN + '&response=' + obtainedToken)
             .then((googleResponse) => {
                 if (googleResponse.data['success'] === true) {
-                    log.debug("Message verified successful", googleResponse.data);
+                    log.debug("Message verified successfully", googleResponse.data);
                     processMessages(expressRequest, expressResponse);
                 } else {
                     log.debug("Message can not be verified", googleResponse.data);
@@ -42,15 +63,22 @@ const sendMail = (expressRequest, expressResponse) => {
             log.error("Can not verify user", error);
             expressResponse.status(500).send(JSON.stringify(error));
         });
+
     } else {
-        log.warn("The current mode allow every message to reach the user. If this is not intended, please turn the filter on to avoid spam.");
+        log.warn("The current mode allows every message to reach the user. If this is not intended, please turn the filter on to avoid spam.");
         log.debug("Incoming message does not have to be verified. Process to save message in database and send message to owner");
         processMessages(expressRequest, expressResponse);
     }
 }
 
+/**
+ * uses func notify to send mail to website owner with nodemailer
+ * if successful the message is saved to the database
+ * @param expressRequest contains message from contact form
+ * @param expressResponse
+ */
 function processMessages(expressRequest, expressResponse) {
-    notify(expressRequest, expressResponse).then(() => {
+    notify(expressRequest).then(() => {
         log.debug("Notifying user is successful. Process to save Message");
         saveMessage(expressRequest, expressResponse);
     }).catch((mailError) => {
@@ -59,20 +87,16 @@ function processMessages(expressRequest, expressResponse) {
     })
 }
 
-function notify(expressRequest, expressResponse) {
+/**
+ * send mail with contact message to website owner
+ * @param expressRequest contains input from message contact form
+ * @returns {Promise<Cancel> | string | Promise<any> | void}
+ */
+function notify(expressRequest) {
     const senderName = expressRequest.body['sendername'];
     const senderEmail = expressRequest.body['sendermail'];
     const subject = expressRequest.body['subject'];
     const message = expressRequest.body['message'];
-    let transporter = nodemailer.createTransport({
-        host: HOST,
-        port: 587,
-        secure: false,
-        auth: {
-            user: SENDER,
-            pass: EMAILPW,
-        },
-    });
 
     return transporter.sendMail({
         from: SENDER,
@@ -80,9 +104,13 @@ function notify(expressRequest, expressResponse) {
         subject: "Contact email from your website",
         text: `From: ${senderName} \n Mail: ${senderEmail} \n Subject: ${subject} \n Message: ${message}`
     });
-
 }
 
+/**
+ * message is saved in database
+ * @param expressRequest
+ * @param expressResponse
+ */
 function saveMessage(expressRequest, expressResponse) {
     const obtainedToken = expressRequest.body["token"];
     const senderName = expressRequest.body['sendername'];
@@ -106,6 +134,7 @@ function saveMessage(expressRequest, expressResponse) {
             }
         )
 }
+
 
 module.exports = {
     sendMail: sendMail
